acb9ec15d17185798af29b1b70f51eef4ddeb19d
VHromyk <65778221+VHromyk@users.noreply.github.com>
2023-10-03 17:53:49 +0300

    change port

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 import {Server} from 'socket.io';
 import {createServer} from 'http';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const {PORT = 5000} = process.env;
 
 const httpServer = createServer();
 
@@ -10,9 +15,10 @@ const wsServer = new Server(httpServer, {
 })
 
 wsServer.on('connection', (socket)=> {
+    console.log('I am here!!!')
     socket.on('chat-message', data => {
         socket.broadcast.emit('chat-message', data)
     })
 });
 
-httpServer.listen(5000)
+httpServer.listen(PORT)
